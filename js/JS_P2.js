9945e99f0835cd3401fcde3d63370471650d7215
jessicaor <oreonevada@gmail.com>
2019-02-03 21:16:26 +0400

    'f974_modif_footer'

---
+++ js/JS_P2.js
@@ -0,0 +1,17 @@
+function myFunction_1() {
+  var x = document.getElementById("myDIV_1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction_2() {
+  var x = document.getElementById("myDIV_2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
